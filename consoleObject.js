4ebd99dd6fd43fd47bc8feab3a99310cbcbd627a
Bala Vignesh Reddy <reddybalavignesh9979@gmail.com>
2023-01-29 16:55:57 +0530

    Console Object Methods

---
+++ consoleObject.js
@@ -0,0 +1,16 @@
+/*
+Console Object Methods
+The console object has several methods, log being one of them.
+Some of them are as follows:
+
+assert() -> Log a message and stack trace to console if the first argument is false
+clear() -> clears the console
+log() -> output a message to the console
+table() -> Displays a tabular data
+warn() -> Used for warnings
+error() -> used for errors
+info() -> used for special information
+
+https://developer.mozilla.org/en-US/docs/Web/API/console
+
+*/
