79707244edcf562cc041931fabbd53744506dd15
Bala Vignesh Reddy <reddybalavignesh9979@gmail.com>
2023-01-31 18:36:48 +0530

    initial commit

---
+++ inlethtml.js
@@ -0,0 +1,13 @@
+/* 
+Events and Other DOM properties
+
+Console dir function
+console log show the element DOM tree
+console dir shows the element as an object with its properties
+
+tagName/nodeName
+used to read tag name of an element 
+tagName -> only exists for element node
+nodeName -> defined for any node(text, comment etc)
+*/
+
