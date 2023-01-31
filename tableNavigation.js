1c907e0b4701cf2d2d4f78477d27154dd661e6e9
Bala Vignesh Reddy <reddybalavignesh9979@gmail.com>
2023-01-31 13:51:51 +0530

    Table Navigation

---
+++ tableNavigation.js
@@ -0,0 +1,17 @@
+/* 
+Table Links
+Certain DOM(Document Object Model) elements may provide additional properties specific to their type.
+Table element supports the following properties
+
+table.rows -> collection of tr elements
+table.contain -> reference to <caption>
+table.tHead -> reference to <thead>
+table.tFoot -> reference to <tfoot>
+table.tBodies -> collection of <tbody>
+tbody.rows -> collection of <tr> inside
+
+tr.cells -> collectio of td and th
+tr.sectionRowIndex -> Index of tr instide enclosing element
+tr.rowIndex -> Row number starting from 0
+td.cellIndex -> No. of cells inside enclosing <tr>
+*/
