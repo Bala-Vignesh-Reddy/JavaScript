5786a134600213af2fe7e061af31cf884b3e0b89
Bala Vignesh Reddy <reddybalavignesh9979@gmail.com>
2023-02-02 15:53:51 +0530

    callback

--- callbacks.js
+++ callbacks.js
@@ -14,4 +14,13 @@ console.log('end')
 /*Callback function
 It is a function passed into another function as an argument, which is then invoked inside the outer function to complete an action.
 
+
+function loadScript (src, Callback){
+    let script = document.createElement('script')
+    script.src = src
+    script.onload = () => Callback(script)
+    document.head.append(script)
+}
+
+ x
 */
