1473cd877badc744e6264a48ce955bfe14de3d38
Balavignesh <108361665+Bala-Vignesh-Reddy@users.noreply.github.com>
2023-02-12 12:52:51 +0530

    JavaScript

--- JavaScript Projects/Color-Flipper/script.js
+++ JavaScript Projects/Color-Flipper/script.js
@@ -19,7 +19,6 @@ function getRandomNumber() {
 }
 
 //for limited colors
-
 // const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
 // const btn = document.getElementById("btn");
 // const color = document.querySelector(".color");
