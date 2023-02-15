46d1eba2206acf7c47de929e82fa7c4b28bf306e
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-15 23:05:42 +0530

    Callback

--- callbacks.js
+++ callbacks.js
@@ -32,12 +32,37 @@ the solution of callback hell is promises.
 
 A promise is a "promise of code execution". The code either executes or fails, in both the cases the user will be notified.
 
-let promise = new Promise(function(resolve, reject){
+let promise = new Promise((resolve, reject)=>{
     //execution
 });
+
+let promise = new Promise(function(resolve, reject){
+    //codes  for execution
+})
 
 promise is used to run the command parallely
 
 .then() -> can be used to get the value
 .catch() -> can be used to catch the error
+
+//we can use loadscript with promises so that we can come over the dom of call back
+*/
+
+
+/*Multiple handlers using promises
+
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        return 1;
+    }, 2000)
+})
+p1.then(()=>{
+    console.log("hello");
+})
+p1.then(()=>{
+    console.log("i am second");
+})
+
+isme .then line wise execute hoga
+
 */
