d9fd542f5f761aa21cb2ffdab60ef7835047b12b
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-19 19:54:59 +0530

    Regular Expression

---
+++ Advanced/regularExpression.js
@@ -0,0 +1,7 @@
+//visit this link to know more aboue Regular Expressson
+//There are many tags which can be seen from the link and explore all the expression
+// regexr.com
+
+const regex = /very/g // this is regular expression 
+const text = "Harry is a very very nice awesome nice very boy"
+console.log(text.replace(regex, "GOOD"))
