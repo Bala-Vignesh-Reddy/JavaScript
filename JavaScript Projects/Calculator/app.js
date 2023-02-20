57824d9783bd3bd6ec69a6cbf448cae8417adf46
reddybalavignesh9979@gmail.com <reddybalavignesh9979@gmail.com>
2023-02-20 11:53:49 +0530

    JavaScript

--- JavaScript Projects/Calculator/app.js
+++ JavaScript Projects/Calculator/app.js
@@ -1,1 +1,11 @@
 let string = ""
+
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        console.log(e.target);
+        string += e.target.innerHTML;
+        document.querySelector("input").value = string
+    })
+})
