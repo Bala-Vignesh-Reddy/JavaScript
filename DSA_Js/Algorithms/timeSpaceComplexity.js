c1630b81ba101e0bbd8b6a2f9ec720ec2ccefa4d
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-10 18:17:42 +0530

    Time and Space Complexity

--- DSA_Js/Algorithms/timeSpaceComplexity.js
+++ DSA_Js/Algorithms/timeSpaceComplexity.js
@@ -19,5 +19,37 @@ function summation(n) {
 let ans = summation(5);
 console.log(ans);
 
-// Big-O notation for above code O(n) -> linear 
+ Big-O notation for above code O(n) -> linear used for space
+ O(1) -> constant  used in space 
+ O(n^2) -> quadratic
+ O(n^3) -> cubic
+ O(logn) -> logarithmic used in space 
+
+
+
+Objects - Big-O 
+Insert - O(1)
+Remove - O(1)
+Access - O(1)
+Search - O(n)
+
+Object.keys() - O(n)
+Object.values() - O(n)
+Object.entries() - O(n)
+
+
+Array - Big-O 
+ Insert/ remove at end -O(1)
+ Insert/remove at beginning - O(n)
+ Access - O(1)
+ Search -O(n)
+ Push/pop - O(1)
+ Shift/unshift/concat/slice/splice - O(n)
+ forEach/map/filter/reduce - O(n)
+
+
+
+
+
+
 
