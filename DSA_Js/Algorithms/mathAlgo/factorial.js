b1ca3faa1a2a622a05e412e9d5ef9174c3fda33a
BalaVignesh <reddybalavignesh9979@gmail.com>
2023-03-13 21:36:18 +0530

    Factorial

---
+++ DSA_Js/Algorithms/mathAlgo/factorial.js
@@ -0,0 +1,12 @@
+const factorial = (n) => {
+  let fact = 1;
+  for(let i=1; i<=n; i++){
+    fact *= i;
+  }
+  return fact;
+}
+
+console.log(factorial(5));
+console.log(factorial(10)); 
+
+Big-O -> O(n) 
